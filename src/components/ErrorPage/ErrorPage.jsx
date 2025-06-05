17c9653fe788f774ea1911f4c68afed2273ad83e
CopyypoC <william.wang650@gmail.com>
2025-06-05 04:11:02 -0700

    feat: add ErrorPage component

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,3 @@
+export function ErrorPage() {
+  return <div data-testid="ErrorPage">Error</div>;
+}
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,11 +2,13 @@ import App from "../components/App/App.jsx";
 import { Home } from "../components/Home/Home.jsx";
 import { Shop } from "../components/Shop/Shop.jsx";
 import { Cart } from "../components/Cart/Cart.jsx";
+import { ErrorPage } from "../components/ErrorPage/ErrorPage.jsx";
 
 export const routes = [
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
