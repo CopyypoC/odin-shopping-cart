ecc76ad4c4d6b1fa0ad16b0e67b065f168b2e573
CopyypoC <william.wang650@gmail.com>
2025-06-04 21:28:06 -0700

    test: add Navar routing tests

--- tests/Navbar.test.jsx
+++ tests/Navbar.test.jsx
@@ -1,22 +1,40 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
+import {
+  createMemoryRouter,
+  RouterProvider,
+  MemoryRouter,
+} from "react-router-dom";
 import { Navbar } from "../src/components/Navbar/Navbar.jsx";
+import { routes } from "../src/routes/routes.jsx";
 
 describe("Navbar", () => {
   it("renders Home, Shop, and Cart link text", () => {
-    render(<Navbar />);
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    );
     expect(screen.getByRole("link", { name: /home/i })).toBeInTheDocument();
     expect(screen.getByRole("link", { name: /shop/i })).toBeInTheDocument();
     expect(screen.getByRole("link", { name: /cart/i })).toBeInTheDocument();
   });
 
-  it("snapshot", () => {
-    const nav = render(<Navbar />);
+  it("matches snapshot", () => {
+    const nav = render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    );
     expect(nav).toMatchSnapshot();
   });
 
   it("links have correct hrefs", () => {
-    render(<Navbar />);
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    );
     expect(screen.getByRole("link", { name: /home/i })).toHaveAttribute(
       "href",
       "/"
@@ -32,15 +50,51 @@ describe("Navbar", () => {
   });
 });
 
-// GENERAL NAV TESTS
-// See if the component renders, snapshot
-// See if elements have the right hrefs
-// See if text inside tag matches expected, "Home" "Shop" "Cart"
+describe("Navbar routes", () => {
+  vi.mock("../src/components/Home/Home.jsx", () => ({
+    Home: () => <div data-testid="HomeMock"></div>,
+  }));
 
-// USER EVENT TEST
-// Note - Dynamic content shouldn't be tested for exact matching since it changes
-// only static content should be checked exact matching like above
-// Clicking on link brings you the right url
-// Match snapshot for the new component that should be rendered
-// Continue with same tests as needed for text matching
-// Attributes etc.
+  vi.mock("../src/components/Shop/Shop.jsx", () => ({
+    Shop: () => <div data-testid="ShopMock"></div>,
+  }));
+
+  vi.mock("../src/components/Cart/Cart.jsx", () => ({
+    Cart: () => <div data-testid="CartMock"></div>,
+  }));
+
+  it("renders Navbar and Home on /", () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/"],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    expect(screen.getByTestId("Navbar")).toBeInTheDocument();
+    expect(screen.getByTestId("HomeMock")).toBeInTheDocument();
+  });
+
+  it("renders Navbar and Shop on /shop", () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/shop"],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    expect(screen.getByTestId("Navbar")).toBeInTheDocument();
+    expect(screen.getByTestId("ShopMock")).toBeInTheDocument();
+  });
+
+  it("renders Navbar and Cart on /cart", () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/cart"],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    expect(screen.getByTestId("Navbar")).toBeInTheDocument();
+    expect(screen.getByTestId("CartMock")).toBeInTheDocument();
+  });
+
+  it("renders error page on bad url", () => {});
+});
