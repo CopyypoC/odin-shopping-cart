c1ce6ad780b2d8f43d7c2cca8ec41d3579e8a494
CopyypoC <william.wang650@gmail.com>
2025-06-14 16:37:02 -0700

    test: add ProductCard amount change tests

--- tests/ProductCard.test.jsx
+++ tests/ProductCard.test.jsx
@@ -1,15 +1,22 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { ProductCard } from "../src/components/Shop/ProductCard/ProductCard.jsx";
 import { render, screen } from "@testing-library/react";
+import { Shop } from "../src/components/Shop/Shop.jsx";
+import userEvent from "@testing-library/user-event";
+import { act } from "react";
 
-const mockProduct = {
-  id: 1,
-  title: "title",
-  price: 1,
-  description: "description",
-  category: "category",
-  image: "url",
-};
+const mockProductList = [
+  {
+    id: 1,
+    title: "title",
+    price: 1,
+    description: "description",
+    category: "category",
+    image: "url",
+  },
+];
+
+const mockProduct = mockProductList[0];
 
 describe("ProductCard component", () => {
   it("renders Add to cart button", () => {
@@ -32,8 +39,61 @@ describe("ProductCard component", () => {
 
     expect(screen.getByText(mockProduct.title)).toBeInTheDocument();
     expect(screen.getByText("$" + mockProduct.price)).toBeInTheDocument();
-    expect(screen.getByText(mockProduct.description)).toBeInTheDocument();
     expect(screen.getByText(mockProduct.category)).toBeInTheDocument();
     expect(screen.getByAltText(mockProduct.title)).toBeInTheDocument();
+  });
+
+  it("increases product amount by 1 when clicking +", async () => {
+    const user = userEvent.setup();
+
+    globalThis.fetch = vi.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve([mockProduct]),
+      })
+    );
+
+    await act(async () => {
+      render(<Shop />);
+    });
+
+    let amount = Number(screen.getByTestId("ProductAmount").textContent);
+    const plusBtn = screen.getByRole("button", { name: "+" });
+
+    expect(amount).toBe(0);
+    await user.click(plusBtn);
+    amount = Number(screen.getByTestId("ProductAmount").textContent);
+    expect(amount).toBe(1);
+  });
+
+  it("decrease product amount by 1 when clicking -", async () => {
+    const user = userEvent.setup();
+
+    globalThis.fetch = vi.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve([mockProduct]),
+      })
+    );
+
+    await act(async () => {
+      render(<Shop />);
+    });
+
+    let amount = Number(screen.getByTestId("ProductAmount").textContent);
+    const plusBtn = screen.getByRole("button", { name: "+" });
+    const minusBtn = screen.getByRole("button", { name: "-" });
+    await user.click(plusBtn);
+
+    amount = Number(screen.getByTestId("ProductAmount").textContent);
+    expect(amount).toBe(1);
+
+    await user.click(minusBtn);
+    amount = Number(screen.getByTestId("ProductAmount").textContent);
+    expect(amount).toBe(0);
+
+    await user.click(minusBtn);
+    amount = Number(screen.getByTestId("ProductAmount").textContent);
+    expect(amount).toBe(0);
   });
 });
