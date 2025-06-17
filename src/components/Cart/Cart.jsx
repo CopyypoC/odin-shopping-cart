429db43ec7c8348cbf6203ccc63a120a1ad897cd
CopyypoC <william.wang650@gmail.com>
2025-06-16 19:48:28 -0700

    feat(Cart): add testid

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,12 @@
 export function Cart() {
   return (
-    <div>
+    <div data-testid="Cart">
       <p>Cart</p>
     </div>
   );
 }
+
+// Maps over cartItems
+// CartItem component
+// Allow amount editing on cart items?
+// Checkout button
